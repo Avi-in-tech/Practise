4a9ed68646350000de92bdb0353744d172175058
tcs45 <tcs45@AVI_PC>
2025-07-28 14:11:19 +0530

    Java batch first commit

---
+++ JavaBatch/src/Package1/Airthematic.java
@@ -0,0 +1,37 @@
+package Package1;
+
+/*
+Assignment1---((((10+2)+2)-2)*2)/2)
+
+Assignment2-- ((((10/2)-2)+2)-2)*2)
+
+*/
+
+public class Airthematic {
+    public int Sum(int a,int b){
+        return (a+b);
+    }
+    public int Sub(int a,int b){
+        return (a-b);
+    }
+    public int Mul(int a,int b){
+        return (a*b);
+    }
+    public int Div(int a,int b){
+        return (a/b);
+    }
+
+    public static void main(String[] args) {
+        Airthematic res = new Airthematic();
+
+        //Assignment1---((((10+2)+2)-2)*2)/2)
+
+        int result1 = res.Div(res.Mul(res.Sub(res.Sum(res.Sum(10,2),2),2),2),2);
+        System.out.println(result1);
+
+       // Assignment2-- ((((10/2)-2)+2)-2)*2)
+        int result2 = res.Mul(res.Sub(res.Sum(res.Sub(res.Div(10,2),2),2),2),2);
+        System.out.println(result2);
+
+    }
+}
---
+++ JavaBatch/src/Package1/Student.java
@@ -0,0 +1,36 @@
+package Package1;
+
+
+
+/*
+ Assignment 1 - Create a class named Student
+
+put two variables
+put two methods----print some message
+
+Create two objects of the class
+call the variables and methods with the help of those two objects...
+*/
+
+public class Student {
+
+      String name = "amit" ;
+      int rollNo = 2;
+      public void getRollNo(){
+          System.out.println("Roll no is " + rollNo);
+      }
+      public void getName(){
+          System.out.println("Name is " + name);
+      }
+
+    public static void main(String[] args) {
+        Student a = new Student();
+        //calling variable
+        System.out.println(a.name +"   "+  a.rollNo);
+        a.getName();
+        a.getRollNo();
+
+    }
+
+}
+
